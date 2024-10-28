e476a415e4c8aac63d5751041477eb9169575a44
Same <135613676+Lazydoz@users.noreply.github.com>
2024-10-28 07:11:28 +0000

    update ver 1.04

--- script/appear.js
+++ script/appear.js
@@ -1,10 +1,27 @@
 let type = document.title;
-
 const file = ("/data/"+type+".json");
+const appear_position = document.querySelector("#container");
 
 fetch(file)
     .then (response => response.json())
     .then (data => {
-        const start = 0;
-        const end = 0;
+        if(Array.isArray(data)){const lenght = data.length;}
+        let start = 0, end = 20;
+
+        for(start; start < end; start++)
+        {
+            const obj = data[start];
+            const appear_obj = document.createElement("div"); appear_obj.classList.add("asset");
+
+            appear_obj.innerHTML = `
+                    <img src="${obj.img}" alt="">
+                    <div class="information">${obj.name}</div>
+                    <div class="source">
+                        <a href="${obj.download}" target="_blank"><i class="fa-solid fa-download"></i></a>
+                        <a href="${obj.source}" target="_blank"><i class="fa-solid fa-link"></i></a>
+                    </div>
+                `;
+
+            appear_position.appendChild(appear_obj);
+        }
     })
